4ca25a0249819154ad84a7c301746203af663e91
Sven Fuchs <svenfuchs@artweb-design.de>
2010-05-01 15:16:56 +0200

    returning empty attrs breaks more than it fixes

--- lib/taka/dom/attr_node_map.rb
+++ lib/taka/dom/attr_node_map.rb
@@ -5,15 +5,15 @@ module Taka
       
       attr_accessor :_node
       
-      def createAttribute name
-        Nokogiri::XML::Attr.new(document, name)
-      end
-      
-      def getNamedItem name
-        item = self[name] || createAttribute(name)
-        item._node_map = self
-        document.decorate(item)
-      end
+      # def createAttribute name
+      #   Nokogiri::XML::Attr.new(document, name)
+      # end
+      #
+      # def getNamedItem name
+      #   item = self[name] || createAttribute(name)
+      #   item._node_map = self
+      #   document.decorate(item)
+      # end
       
       def setNamedItem item
         if item._node_map && item._node_map != self
